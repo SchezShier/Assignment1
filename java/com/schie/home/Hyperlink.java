a1c50e0632fb99be1f7a5cd89042bd6b8cf7e206
Schez <84888674+SchezShier@users.noreply.github.com>
2022-12-16 17:13:33 +0800

    Add files via upload

---
+++ java/com/schie/home/Hyperlink.java
@@ -0,0 +1,14 @@
+package com.schie.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Hyperlink extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hyperlink);
+    }
+}
